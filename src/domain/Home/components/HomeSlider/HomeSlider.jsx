9b4c14cffb2dcd7d9e2cade18f0cf9c6426c53a3
Konrad Knochenhauer <103902628+konradkn@users.noreply.github.com>
2025-01-02 14:16:12 -0600

    update: carousel img

--- src/domain/Home/components/HomeSlider/HomeSlider.jsx
+++ src/domain/Home/components/HomeSlider/HomeSlider.jsx
@@ -4,16 +4,16 @@ const HomeSlider = ({ img1, img2, img3, img4}) => {
   return (
     <>
       <div className="bg-slate-800 w-full h-96 rounded-3xl carousel">
-        <div className="carousel-item">
+        <div className="carousel-item w-full">
           <img src={img1} />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item w-full">
           <img src={img2} />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item w-full">
           <img src={img3} />
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item w-full">
           <img src={img4} />
         </div>
       </div>
